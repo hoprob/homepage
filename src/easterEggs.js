b41e5de195f1def9cda52a95f202f8fdf7b81848
Robin Svensson <robinesvensson@gmail.com>
2022-05-12 13:44:26 +0200

    Changed Eastereggs methods so that I could remove event listeners between each component.

--- src/easterEggs.js
+++ src/easterEggs.js
@@ -1,40 +1,46 @@
-let press;
-let click;
+
 
 //Click easteregg
 export const EasterEgg1 = () => {
-    document.querySelector('.header').addEventListener('click', click => {
-        if(click.clientY > 60 && click.clientY < 90 && click.clientX >  70 && click.clientX <  100){
-            if(document.querySelector('.header').style.backgroundImage === "" || document.querySelector('.header').style.backgroundImage === "linear-gradient(black, rgb(54, 54, 54))"){
-                document.querySelector('.header').style.background = "linear-gradient(red, rgb(54, 54, 54))";
-            }else{
-                document.querySelector('.header').style.background = "linear-gradient(black, rgb(54, 54, 54))";
-            }
+    
+    console.log('Start Easteregg 1');
+    document.querySelector('.header').addEventListener('click', handleClick)
+}
+function handleClick(e){
+    if(e.clientY > 60 && e.clientY < 100 && e.clientX >  70 && e.clientX <  100){
+        if(document.querySelector('.header').style.backgroundImage === "" || document.querySelector('.header').style.backgroundImage === "linear-gradient(black, rgb(54, 54, 54))"){
+            document.querySelector('.header').style.background = "linear-gradient(red, rgb(54, 54, 54))";
+        }else{
+            document.querySelector('.header').style.background = "linear-gradient(black, rgb(54, 54, 54))";
         }
-    })
+    }
 }
 
 //Keybord Easteregg
+let pressedKeys = [];
+const keyWord = 'hej';
 export const EasterEgg2 = () => {
-    let pressedKeys = [];
-    const keyWord = 'hej';
-    document.addEventListener('keypress', press =>{
-    console.log(press.key)
+    console.log('Start Easteregg 2');
+    document.addEventListener('keypress', handleKeyPress)
+}
+function handleKeyPress(e){
+    
+    console.log(e.key);
     if(pressedKeys.length >= keyWord.length){
         for(let i = 0; i < keyWord.length; i++){
             pressedKeys[i] = pressedKeys[i+1];
         }
         pressedKeys.pop();
     }
-    pressedKeys.push(press.key);
-    console.log(pressedKeys.join(''))
+    pressedKeys.push(e.key);
+    console.log(pressedKeys.join(''));
     if(pressedKeys.join('') === keyWord){
         alert('     Hej på dig med! :D');
     }
-})
 }
 
 export const CleanEasterEggs = () => {
-    document.removeEventListener('keypress', press);
-    document.removeEventListener('click', click);
+    console.log('Cleaning up from easterEggs');
+    document.removeEventListener('keypress', handleKeyPress);
+    document.removeEventListener('click', handleClick);
 }
